3498cb0100e58eb85a0901594cf1ff7966a634e8
Norris Nicholson <npnicholson@gatech.edu>
2018-02-26 18:55:59 -0500

    Added basic content to help identify layout elements

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $( document ).ready(function(){
   console.log("JS Init");
-  $('#micro_rightTop').html('Hello World');
+  //$('#micro_rightTop').html('Hello World');
   $('#micro_rightTop').css('color','blue');
 });
