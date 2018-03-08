c9869513559ff0773dcb6a2dcb47f7f70e4c024d
Keenan Nicholson <knicholson32@gatech.edu>
2018-03-08 16:59:49 -0500

    Added initial frame for 'configureKeyBindings'.

--- js/keys.js
+++ js/keys.js
@@ -4,4 +4,23 @@
 // TODO: Add fule description here
 // ========================================================== //
 
-var KEY_TYPE = {EDITOR:0, TREE:1};
+const KEY_TYPE = {EDITOR:0, TREE:1};
+
+var configureKeyBindings = function(key_type, target){
+  switch(key_type){
+    case KEY_TYPE.EDITOR:
+      Mousetrap.bind('meta+s', function() {
+        console.log('File Save');
+        return false;
+      });
+      break;
+    case KEY_TYPE.TREE:
+      Mousetrap.bind('meta+s', function() {
+        console.log('File Save on Tree');
+        return false;
+      });
+      break;
+    default:
+      break;
+  }
+}
