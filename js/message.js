d31b6999a8ef87a9a1e0b00f67f4991f862254c2
Keenan Nicholson <knicholson32@gatech.edu>
2018-03-15 16:08:44 -0400

    Edited for new MicroServer changes.

--- js/message.js
+++ js/message.js
@@ -4,29 +4,33 @@
 // TODO: Add fule description here
 // ========================================================== //
 
-var socket_pass = getParameterByName('key');
+var socket_user = 'user1';
+var socket_pass = 'pass1';
 var socket_connected = false;
 var socket_refreshFileTree;
 var hash = undefined;
-function socketInit(callback){
+
+function socketInit(callback) {
   hash = undefined;
-  if(socket_connected){
+  if (socket_connected) {
     callback();
-  }else{
+  } else {
     micro.conn = new WebSocket(getParameterByName('socket'));
-    micro.conn.onmessage = function(e){
+    micro.conn.onmessage = function(e) {
       processMessage(e);
     }
-    micro.conn.onopen = function(e){
+    micro.conn.onopen = function(e) {
       callback();
       socket_connected = true;
     }
     //micro.conn.onclose = micro.initFileSystem(callback);
-    micro.conn.onclose = function(e){
+    micro.conn.onclose = function(e) {
       socket_connected = false;
-      setTimeout(function(){socketInit(callback)},500);
+      setTimeout(function() {
+        socketInit(callback)
+      }, 500);
     }
-    micro.conn.onerror = function(e){
+    micro.conn.onerror = function(e) {
       console.error(e);
     }
     socket_refreshFileTree = micro.refreshFileTree;
@@ -38,10 +42,10 @@ function socketInit(callback){
  * run when the transfer is complete. The callback will be passed a file list.
  * :: function(callback)
  */
-function socketGetFileTree(callback){
-  let process = function(msg){
+function socketGetFileTree(callback) {
+  let process = function(msg) {
     files = [];
-    for(let i = 0; i < msg.data.length; i++){
+    for (let i = 0; i < msg.data.length; i++) {
       files.push(msg.data[i]);
     }
     console.log(files);
@@ -58,27 +62,32 @@ function socketGetFileTree(callback){
  * resulting file as well as some meta data.
  * :: function(callback)
  */
-function socketLoadFile(file, callback){
-  let process = function(msg){
+function socketLoadFile(file, callback) {
+  let process = function(msg) {
     let html_file = msg.data.data.replace(/(?:\r\n|\r|\n)/g, '<br />');
-    callback({name:msg.data.name, data:html_file, type:msg.data.type, size:msg.data.length});
+    callback({
+      name: msg.data.name,
+      data: html_file,
+      type: msg.data.type,
+      size: msg.data.length
+    });
   }
   let id = makeid();
   addCallback(process, id);
-  socketSend(['load','utf-8',file.escapeSpecialChars()], id);
+  socketSend(['load', 'utf-8', file.escapeSpecialChars()], id);
 }
 
-function socketSaveFile(filename, filedata, callback){
-  let process = function(msg){
+function socketSaveFile(filename, filedata, callback) {
+  let process = function(msg) {
     callback(msg);
   }
   let id = makeid();
   addCallback(process, id);
-  socketSend(['save', 'utf-8', filename, filedata.escapeSpecialChars()] ,id);
+  socketSend(['save', 'utf-8', filename, filedata.escapeSpecialChars()], id);
 }
 
-function socketDeleteFile(filename, callback){
-  let process = function(msg){
+function socketDeleteFile(filename, callback) {
+  let process = function(msg) {
     callback(msg);
   }
   let id = makeid();
@@ -89,17 +98,17 @@ function socketDeleteFile(filename, callback){
 var socket_key;
 var socket_callback_queue = {};
 
-function addCallback(callback, id){
+function addCallback(callback, id) {
   socket_callback_queue[id] = callback;
 }
 
-function getCallback(id){
+function getCallback(id) {
   let c = socket_callback_queue[id];
   delete socket_callback_queue[id];
   return c;
 }
 
-function processMessage(e){
+function processMessage(e) {
   let msg = JSON.parse(e.data);
   console.log(msg);
   /* Codes:
@@ -112,12 +121,12 @@ function processMessage(e){
    * 6: Key set
    * 7: File/Direcory Update
    */
-  if(msg.code === 1){
+  if (msg.code === 1) {
     var callback = msg.callback;
-    if(callback !== "0"){
+    if (callback !== "0") {
       getCallback(callback)(msg);
     }
-  }else if(msg.code === 7){
+  } else if (msg.code === 7) {
     /*switch(msg.data[2]){
       case 'copy':
       case 'move':
@@ -130,14 +139,14 @@ function processMessage(e){
     let arr = msg.data[0].split('/');
     socket_refreshFileTree({
       path: msg.data[0],
-      filename: arr[arr.length-1],
+      filename: arr[arr.length - 1],
       data: msg.data[1],
-      size: (msg.data[1]!==undefined?msg.data[1].length:0)
+      size: (msg.data[1] !== undefined ? msg.data[1].length : 0)
     });
-  }else if(msg.code === 6){
+  } else if (msg.code === 6) {
     socket_key = msg.data;
-  }else{
-    switch(msg.code){
+  } else {
+    switch (msg.code) {
       case 0:
         console.error('Refused connection: ' + msg.msg);
         break;
@@ -162,13 +171,15 @@ function processMessage(e){
 
 }
 
-function socketSend(rqst, call){
+function socketSend(rqst, call) {
   // If the socket is not connected, send it later
-  if(!socket_connected){
-    setTimeout(function(){socketSend(rqst,call)}, 500);
-  }else{
-    if(hash === undefined){
-      hash = MD5(socket_key + '-' + socket_pass);
+  if (!socket_connected) {
+    setTimeout(function() {
+      socketSend(rqst, call)
+    }, 500);
+  } else {
+    if (hash === undefined) {
+      hash = MD5(socket_user + '_' + socket_pass + '_' + socket_key);
     }
     let to_send = {
       key: hash,
@@ -181,12 +192,12 @@ function socketSend(rqst, call){
 
 
 String.prototype.escapeSpecialChars = function() {
-    return this.replace(/\\n/g, "\\n")
-               .replace(/\\'/g, "\\'")
-               .replace(/\\"/g, '\\"')
-               .replace(/\\&/g, "\\&")
-               .replace(/\\r/g, "\\r")
-               .replace(/\\t/g, "\\t")
-               .replace(/\\b/g, "\\b")
-               .replace(/\\f/g, "\\f");
+  return this.replace(/\\n/g, "\\n")
+    .replace(/\\'/g, "\\'")
+    .replace(/\\"/g, '\\"')
+    .replace(/\\&/g, "\\&")
+    .replace(/\\r/g, "\\r")
+    .replace(/\\t/g, "\\t")
+    .replace(/\\b/g, "\\b")
+    .replace(/\\f/g, "\\f");
 };
